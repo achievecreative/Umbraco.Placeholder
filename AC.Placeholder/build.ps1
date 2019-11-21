463c29bc4e763954f30ce6b5a8315adf2b6b9701
Owen chen <owen.chen@blackmores.com.au>
2019-11-21 15:14:09 +0800

    Update

--- AC.Placeholder/build.ps1
+++
@@ -1,3 +0,0 @@
-$msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe'
-
-& "$($msBuildExe)" ".\AC.Placeholder.csproj"
---
+++ deploy.ps1
@@ -0,0 +1,8 @@
+$UmbracoSite = 'D:\Interestings\Umbraco-8.0.2'
+
+$msBuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe' 
+& "$($msBuildExe)" ".\AC.Placeholder.sln"
+
+Copy-Item .\AC.Placeholder.Features\bin\AC.Placeholder.* "$($UmbracoSite)\bin\"
+
+Copy-Item .\AC.Placeholder.Features\Views\**\* "$($UmbracoSite)\Views"
